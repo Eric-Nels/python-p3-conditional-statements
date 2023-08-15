782e448d0e0dbce572d6022aadac12f38572b6e9
Eric Nelson <ernelson12o0@gmail.com>
2023-08-15 19:54:24 -0400

    Lab Complete

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@
   password. If the username is "admin" or "ADMIN" and the password is "12345",
   return "Access granted". Otherwise, return "Access denied".
 */
+def admin_login(username, password):
+        if (username == 'admin' and password == 12345):
+            return 'Access granted'
+        else: 
+            return  'Access denied'
+
 function adminLogin(username, password) {
   if ((username === "admin" || username === "ADMIN") && password === "12345") {
     return "Access granted";
